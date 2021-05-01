8c996edf209468c148a2bcdd865a8a6665ae00f6
Bofan Chen <bofanchen98@gmail.com>
2021-04-30 20:21:05 -0700

    Added second method.

--- mysql-median.sql
+++ mysql-median.sql
@@ -5,7 +5,7 @@
 -- "A.COLUMN_NAME" is the smaller number, if there is an even amount of values.
 -- "B.COLUMN_NAME" is the larger number, if there is an even amount of values.
 -- Round the median to the thousandth's place.
-SELECT FORMAT((A.COLUMN_NAME+B.COLUMN_NAME)/2,3)
+SELECT FORMAT((A.COLUMN_NAME+B.COLUMN_NAME)/2,3) AS MEDIAN
 FROM TABLE_NAME A, TABLE_NAME B
 WHERE (
     SELECT COUNT(*)
@@ -22,3 +22,20 @@ AND (
     WHERE COLUMN_NAME > B.COLUMN_NAME) = (
     SELECT FLOOR(COUNT(*)/2)
     FROM TABLE_NAME);
+
+/*
+   Improved method.
+ */
+SET @COUNT := (
+    SELECT COUNT(*)
+    FROM TABLE_NAME);
+SET @ROW_I := 0;
+SELECT FORMAT(AVG(COLUMN_NAME),3) AS MEDIAN
+FROM (
+    SELECT *
+    FROM TABLE_NAME
+    ORDER BY COLUMN_NAME) AS ORDERED
+WHERE (
+    SELECT @ROW_I := @ROW_I + 1)
+BETWEEN @COUNT/2.0
+AND @COUNT/2.0 + 1;
